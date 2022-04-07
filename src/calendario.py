79b197ef8487fdb6736070359fac5eba6b096617
fabriciolopss <fabriciolopss@ufmg.br>
2022-04-07 15:51:56 -0300

    Commit projeto quase pronto

---
+++ src/calendario.py
@@ -0,0 +1,54 @@
+import re
+import datetime
+
+class Formatacao():
+    def __init__(self):
+        self.datas = []
+        self.cidade = []
+        self.diaDaSemana = []
+        self.nomeFeriado = []
+        self.data = datetime.datetime.now()
+        self.diaMes = self.data.strftime("%d/%m")
+        self.quantDiaMeses = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+        self.feriadosProximos = {}
+
+    def read_document(self):
+        with open ('src/feriados.txt', 'r', encoding = "utf-8") as f:
+            lines = f.readlines()
+            for line in lines:
+                if (line[0].isnumeric()): #checa se é a linha correspondente a data e cidade
+                    self.datas.append(line[0:5]) #Extraindo data da linha correspondente
+                    self.cidade.append(re.search(r'\((.*?)\)',line).group(1)) #Extraindo cidade da linha correspondente
+                else:
+                    divisao = line.split(", ") #dividindo a linha 2 em antes e depois da virgula
+                    self.nomeFeriado.append(divisao[0]) #atribuindo valores com vetor gerado
+                    self.diaDaSemana.append(divisao[1].strip('\n'))
+        self.getClosest()
+
+    def getClosest(self): #função responsável por pegar todos os feriados e calcular a distancia em dias até ele
+        index = 0
+        for x in self.datas:
+            distancia = 0
+            diaFeriado = int(x[0:2])
+            diaAtual = int(self.diaMes[0:2])
+            mesFeriado = int(x[3:5])
+            mesAtual = int(self.diaMes[3:5])
+            if(mesFeriado == mesAtual):
+                distancia = abs(diaAtual - diaFeriado)
+            elif(mesAtual < mesFeriado):
+                if(diaAtual > diaFeriado):
+                    for x in range(mesAtual, mesFeriado):
+                        distancia += self.quantDiaMeses[x - 1]
+                    distancia -= diaAtual - diaFeriado
+                else:
+                    for x in range(mesAtual, mesFeriado):
+                        distancia += self.quantDiaMeses[x - 1]
+                    distancia += diaFeriado - diaAtual
+            self.feriadosProximos[self.nomeFeriado[index]] = distancia
+            index += 1
+    
+    def feriados(self):
+        self.read_document()
+        return self.feriadosProximos, self.diaDaSemana, self.datas, self.cidade, self.nomeFeriado
+
+
---
+++ twitter.py
@@ -0,0 +1,62 @@
+import tweepy
+from src.calendario import Formatacao
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class twitter():
+    def __init__(self):
+        self.consumerKey = os.getenv('consumerKey')
+        self.consumerSecret = os.getenv('consumerSecret')
+        self.acessToken = os.getenv('acessToken')
+        self.acessSecret = os.getenv('acessSecret')
+        self.tweetString = ""
+
+
+    def tweetMaker(self):
+        calendario = Formatacao()
+        distanciaFeriados, diasDaSemana, datas, cidade, nomeFeriado = calendario.feriados()
+        index = 0
+        for x in nomeFeriado:
+            if (0 < distanciaFeriados[x] < 30 and self.tweetString == ""):
+                if(distanciaFeriados[x] == 1):
+                    if (diasDaSemana[index] == "Domingo" or diasDaSemana[index] == "Sábado" ):
+                        self.tweetString = "O feriado amanhã!! Infelizmente ele caiu em final de semana."
+                    else:
+                        self.tweetString = "O feriado começa amanhã, vamos comemorar!"
+                else:
+                    if (diasDaSemana[index] == "Domingo" or diasDaSemana[index] == "Sábado" ):  
+                        if(cidade[index] == "Geral"):                
+                            self.tweetString += "Faltam apenas {} dias para o próximo feriado ({}), que (infelizmente) cairá em um final de semana ({})."\
+                            "\nEsse feriado é tanto pro campus Belo Horizonte quanto para Montes Claros. \n".format(distanciaFeriados[x], nomeFeriado[index], diasDaSemana[index])
+                        else:
+                            self.tweetString += "Faltam apenas {} dias para o próximo feriado ({}), que cairá em um dia de semana ({})."\
+                            "\nEsse feriado é pro campus {}. \n".format(distanciaFeriados[x], nomeFeriado[index], diasDaSemana[index], cidade[index])                        
+                    else:
+                        if(cidade[index] == "Geral"):
+                            self.tweetString += "Faltam apenas {} dias para o próximo feriado ({}), que cairá em um dia de semana ({}-feira)."\
+                            "\nEsse feriado é tanto pro campus Belo Horizonte quanto para Montes Claros. \n".format(distanciaFeriados[x], nomeFeriado[index], diasDaSemana[index])
+                        else:
+                            self.tweetString += "Faltam apenas {} dias para o próximo feriado ({}), que cairá em um dia de semana ({}-feira)."\
+                            "\nEsse feriado é pro campus {}. \n".format(distanciaFeriados[x], nomeFeriado[index], diasDaSemana[index], cidade[index])
+
+            index += 1
+        if(self.tweetString == ""):
+            self.tweetString = "Não existe nenhum feriado nos próximos 30 dias :("
+        print(self.tweetString)
+
+    def useTwitter(self, auth):
+        api = tweepy.API(auth)
+        api.update_status(status = self.tweetString)
+
+
+    def loginTwitter(self):
+        auth = tweepy.OAuthHandler(self.consumerKey, self.consumerSecret)
+        auth.set_access_token(self.acessToken, self.acessSecret)
+        self.useTwitter(auth)
+        self.tweetMaker()
+
+
+twettar = twitter()
+twettar.loginTwitter()
